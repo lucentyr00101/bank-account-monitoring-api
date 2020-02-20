50fee287ebc8420d5407548653922b9ea2491fa1
Ranz Sarcos <rsarcos@yondu.com>
2020-02-20 11:28:53 +0800

    init

---
+++ app/Http/Controllers/BankDetailController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BankDetailController extends Controller
+{
+    public function index() {
+        
+    }
+}
---
+++ database/migrations/2020_02_13_053956_create_bank_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('bank');
+            $table->string('account_type');
+            $table->string('account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_details');
+    }
+}
