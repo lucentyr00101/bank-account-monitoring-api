cf57a15873a3f9c7a78e7c86e7318289f731a534
Ranz Sarcos <rsarcos@yondu.com>
2020-02-27 11:33:07 +0800

    added resource for bank details, and modified user resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 });
