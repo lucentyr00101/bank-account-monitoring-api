df93597f010b4297740bb5e319e8d3f75fdf4348
Ranz Sarcos <rsarcos@yondu.com>
2020-03-02 14:33:20 +0800

    added name manipulation in user model

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,9 +17,10 @@ class UserResource extends JsonResource
     {
         return [
             'id'           => $this->id,
-            'name'         => $this->first_name,
+            'first_name'         => $this->first_name,
             'middle_name'  => $this->middle_name,
             'last_name'    => $this->last_name,
+            'full_name'    => $this->fullName,
             'birthday'     => $this->birthday,
             'email'        => $this->email,
             'bank_details' => new BankDetailResource($this->bankDetails),
