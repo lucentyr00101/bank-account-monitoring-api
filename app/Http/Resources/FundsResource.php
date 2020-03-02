512e8cbaa93f269762c859c8e910d04e03f9a0df
Ranz Sarcos <rsarcos@yondu.com>
2020-03-02 14:41:17 +0800

    fixed date format in funds resource

--- app/Http/Resources/FundsResource.php
+++ app/Http/Resources/FundsResource.php
@@ -19,7 +19,7 @@ class FundsResource extends JsonResource
             'id' => $this->id,
             'user' => new UserResource($this->user),
             'amount' => $this->amount,
-            'created_at' => $this->created_at,
+            'created_at' => $this->created_at->format('F d, Y h:i:s A'),
         ];
     }
 }
